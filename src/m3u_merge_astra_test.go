c892f668b950538e096a6501a0f2ef4fe2b0da26
SCP002 <feedusfeedme@gmail.com>
2023-02-21 21:37:24 +0300

    Add actual data to TestMainCrash()

--- src/m3u_merge_astra_test.go
+++ src/m3u_merge_astra_test.go
@@ -16,13 +16,42 @@ func TestMainCrash(t *testing.T) {
 	assert.NoError(t, err, "should copy default program config")
 
 	m3uPath := filepath.Join(t.TempDir(), "m3u_merge_astra_main_test.m3u")
-	m3uBytes := []byte(``)
+	m3uBytes := []byte(`#EXTM3U
+	#EXTINF:-1 group-title="Group 3",Channel 3
+	http://url/3
+	#EXTINF:-1 group-title="Group 4",Channel 4
+	http://url/4`)
 
 	err = os.WriteFile(m3uPath, m3uBytes, 0644)
 	assert.NoError(t, err, "should write m3u file")
 
 	astraCfgInputPath := filepath.Join(t.TempDir(), "m3u_merge_astra_main_test.json")
-	astraCfgInputBytes := []byte(`{}`)
+	astraCfgInputBytes := []byte(`{
+		"categories": [
+		  {
+			"name": "Category 1",
+			"groups": [{ "name": "Group 1" }, { "name": "Group 2" }]
+		  }
+		],
+		"make_stream": [
+		  {
+			"enable": true,
+			"groups": { "Category 1": "Group 1" },
+			"id": "0001",
+			"input": ["http://url/1"],
+			"name": "Channel 1",
+			"type": "spts"
+		  },
+		  {
+			"enable": true,
+			"groups": { "Category 1": "Group 2" },
+			"id": "0002",
+			"input": ["http://url/2"],
+			"name": "Channel 2",
+			"type": "spts"
+		  }
+		]
+	  }`)
 
 	err = os.WriteFile(astraCfgInputPath, astraCfgInputBytes, 0644)
 	assert.NoError(t, err, "should write input astra config file")
