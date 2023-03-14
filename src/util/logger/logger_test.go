6585ec0d5e3a2fde4f7a2ef7ddf8b79699417b9a
SCP002 <feedusfeedme@gmail.com>
2023-03-14 15:00:31 +0300

    Improve util/logger.TestNew()

--- src/util/logger/logger_test.go
+++ src/util/logger/logger_test.go
@@ -1,17 +1,26 @@
 package logger
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/zenizh/go-capturer"
 )
 
 func TestNew(t *testing.T) {
-	log := New(logrus.DebugLevel)
-	log.Debug("Debug message")
-	log.Info("Info message")
-	log.Warning("Warning message")
-	log.Error("Error message")
-	assert.Panics(t, func() { log.Panic("Panic message") }, "should panic")
+	out := capturer.CaptureStderr(func() {
+		log := New(logrus.DebugLevel)
+		log.Debug("message")
+		log.Info("message")
+		log.Warning("message")
+		log.Error("message")
+		assert.Panics(t, func() { log.Panic("message") }, "should panic")
+	})
+	assert.Regexp(t, regexp.MustCompile(`\[.*\] DEBUG message`), out)
+	assert.Regexp(t, regexp.MustCompile(`\[.*\]  INFO message`), out)
+	assert.Regexp(t, regexp.MustCompile(`\[.*\]  WARN message`), out)
+	assert.Regexp(t, regexp.MustCompile(`\[.*\] ERROR message`), out)
+	assert.Regexp(t, regexp.MustCompile(`\[.*\] PANIC message`), out)
 }
