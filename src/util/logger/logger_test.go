44413c5ae7d6b3f89de5c6cc5b1d50371e11db72
SCP002 <feedusfeedme@gmail.com>
2023-03-18 14:16:33 +0300

    Add trace level to util/logger.TestNew()

--- src/util/logger/logger_test.go
+++ src/util/logger/logger_test.go
@@ -12,12 +12,14 @@ import (
 func TestNew(t *testing.T) {
 	out := capturer.CaptureStderr(func() {
 		log := New(logrus.DebugLevel)
+		log.Trace("message")
 		log.Debug("message")
 		log.Info("message")
 		log.Warning("message")
 		log.Error("message")
 		assert.Panics(t, func() { log.Panic("message") }, "should panic")
 	})
+	assert.NotRegexp(t, regexp.MustCompile(`\[.*\] TRACE message`), out, "should not print messages with trace level")
 	assert.Regexp(t, regexp.MustCompile(`\[.*\] DEBUG message`), out)
 	assert.Regexp(t, regexp.MustCompile(`\[.*\]  INFO message`), out)
 	assert.Regexp(t, regexp.MustCompile(`\[.*\]  WARN message`), out)
