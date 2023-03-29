72ec04b516e576efec2742228fb9b7120cfd0738
SCP002 <feedusfeedme@gmail.com>
2023-03-29 13:56:21 +0300

    Create wrapper for logger with Infoc() and Debugc() methods

--- src/util/logger/logger.go
+++ src/util/logger/logger.go
@@ -1,15 +1,24 @@
 package logger
 
 import (
+	"fmt"
 	"os"
+	"runtime"
+	"strings"
 	"time"
 
+	"github.com/fatih/color"
 	"github.com/sirupsen/logrus"
 	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
 
+// Logger represents wrapper over logrus
+type Logger struct {
+	*logrus.Logger
+}
+
 // New returns new configured logger
-func New(lvl logrus.Level) *logrus.Logger {
+func New(lvl logrus.Level) *Logger {
 	formatter := prefixed.TextFormatter{
 		FullTimestamp:   true,
 		TimestampFormat: time.Stamp,
@@ -21,5 +30,47 @@ func New(lvl logrus.Level) *logrus.Logger {
 		Level:     lvl,
 		Hooks:     make(logrus.LevelHooks),
 	}
-	return &log
+	return &Logger{&log}
+}
+
+// Infoc prints info level <msg> with formatted and colored <fields> 
+func (l Logger) Infoc(msg string, fields ...any) {
+	l.Infof("%v: %v", msg, buildFields(fields))
+}
+
+// Debugc prints debug level <msg> with formatted and colored <fields>.
+//
+// <msg> is prefixed with caller info.
+func (l Logger) Debugc(msg string, fields ...any) {
+	msgWithCaller := fmt.Sprintf(`(%v): %v`, getCallerInfo(2), msg)
+
+	l.Debugf("%v: %v", msgWithCaller, buildFields(fields))
+}
+
+// buildFields returns comma separated <fields> with every second field quoted and colored
+func buildFields(fields []any) (out string) {
+	cyan := color.New(color.FgHiCyan).SprintFunc()
+
+	for i, field := range fields {
+		fieldStr := fmt.Sprint(field)
+		if fieldStr == "" {
+			continue
+		}
+		if i % 2 == 0 {
+			out += fieldStr + " "
+		} else {
+			out += `"` + cyan(fieldStr) + `"`
+			if i < len(fields) - 1 {
+				out += ", "
+			}
+		}
+	}
+
+	return strings.TrimSpace(out)
+}
+
+// getCallerInfo returns runtime caller info with amount of stack frames to <skip> or empty string on error
+func getCallerInfo(skip int) string {
+	pc, _, line, _ := runtime.Caller(skip)
+	return fmt.Sprintf(`%v; L%v`, runtime.FuncForPC(pc).Name(), line)
 }
--- src/util/logger/logger_test.go
+++ src/util/logger/logger_test.go
@@ -19,10 +19,45 @@ func TestNew(t *testing.T) {
 		log.Error("message")
 		assert.Panics(t, func() { log.Panic("message") }, "should panic")
 	})
-	assert.NotRegexp(t, regexp.MustCompile(`\[.*\] TRACE message`), out, "should not print messages with trace level")
+	msg := "should not print trace messages with debug level logger"
+	assert.NotRegexp(t, regexp.MustCompile(`\[.*\] TRACE message`), out, msg)
 	assert.Regexp(t, regexp.MustCompile(`\[.*\] DEBUG message`), out)
 	assert.Regexp(t, regexp.MustCompile(`\[.*\]  INFO message`), out)
 	assert.Regexp(t, regexp.MustCompile(`\[.*\]  WARN message`), out)
 	assert.Regexp(t, regexp.MustCompile(`\[.*\] ERROR message`), out)
 	assert.Regexp(t, regexp.MustCompile(`\[.*\] PANIC message`), out)
+}
+
+func TestInfoc(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(logrus.InfoLevel)
+
+		log.Infoc("message", "field 1", "value 1", "field 2", 10)
+	})
+	assert.Contains(t, out, `INFO message: field 1 "value 1", field 2 "10"`)
+}
+
+func TestDebugc(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(logrus.DebugLevel)
+
+		log.Debugc("message", "field 1", "value 1", "field 2", 10)
+	})
+	msg := `DEBUG (m3u_merge_astra/util/logger.TestDebugc.func1; L44): message: field 1 "value 1", field 2 "10"`
+	assert.Contains(t, out, msg)
+}
+
+func TestBuildFields(t *testing.T) {
+	assert.Exactly(t, ``, buildFields([]any{}))
+	assert.Exactly(t, ``, buildFields([]any{""}))
+	assert.Exactly(t, `a`, buildFields([]any{"a"}))
+	assert.Exactly(t, `a`, buildFields([]any{"a", ""}))
+	assert.Exactly(t, `"10"`, buildFields([]any{"", 10}))
+	assert.Exactly(t, `"10", b`, buildFields([]any{"", 10, "b", ""}))
+	assert.Exactly(t, `a "10"`, buildFields([]any{"a", 10}))
+	assert.Exactly(t, `a "10", b "c"`, buildFields([]any{"a", 10, "b", "c"}))
+}
+
+func TestGetCallerInfo(t *testing.T) {
+	assert.Exactly(t, `m3u_merge_astra/util/logger.TestGetCallerInfo; L62`, getCallerInfo(1))
 }
