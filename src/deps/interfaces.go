30f7f3e9b680dde9d68cfc13c6f34c8fe973dc63
SCP002 <feedusfeedme@gmail.com>
2023-03-29 13:55:31 +0300

    Remove (mostly) tablewriter

--- src/deps/interfaces.go
+++ src/deps/interfaces.go
@@ -2,7 +2,6 @@ package deps
 
 import (
 	"m3u_merge_astra/cfg"
-	"m3u_merge_astra/util/tw"
 
 	"github.com/sirupsen/logrus"
 )
@@ -10,6 +9,5 @@ import (
 // Global represents global dependencies holder interface
 type Global interface {
 	Log() *logrus.Logger
-	TW() tw.Writer
 	Cfg() cfg.Root
 }
