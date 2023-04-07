f96fd5370a1feb9760ada155b182cad93a63e88f
SCP002 <feedusfeedme@gmail.com>
2023-04-07 18:26:25 +0300

    Set program verison to v1.4.0

--- src/m3u_merge_astra.go
+++ src/m3u_merge_astra.go
@@ -25,7 +25,7 @@ func main() {
 	// Parse command line arguments
 	flags, err := cli.Parse()
 	if flags.Version {
-		fmt.Println("v1.3.1")
+		fmt.Println("v1.4.0")
 		os.Exit(0)
 	}
 	if cli.IsErrOfType(err, goFlags.ErrHelp) {
