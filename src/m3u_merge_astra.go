832f1ca36b4adc39e8aadc284b2aeb4c8ce76459
SCP002 <feedusfeedme@gmail.com>
2023-05-22 12:01:27 +0300

    Increase program version to 1.5.0

--- src/m3u_merge_astra.go
+++ src/m3u_merge_astra.go
@@ -27,7 +27,7 @@ func main() {
 	// Parse command line arguments
 	flags, err := cli.Parse()
 	if flags.Version {
-		fmt.Println("v1.4.1")
+		fmt.Println("v1.5.0")
 		os.Exit(0)
 	}
 	if cli.IsErrOfType(err, goFlags.ErrHelp) {
