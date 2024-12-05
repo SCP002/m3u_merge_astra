079b2e1d1b7d3221c82796e49ce1d97e9239e485
SCP002 <feedusfeedme@gmail.com>
2024-12-06 00:45:46 +0300

    Continue reworking logger

--- src/util/logger/logger.go
+++ src/util/logger/logger.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/cockroachdb/errors"
 	"github.com/fatih/color"
-	pLogger "github.com/phuslu/log"
+	pLog "github.com/phuslu/log"
 	"github.com/samber/lo"
 )
 
@@ -24,33 +24,143 @@ var levelColorMap = map[string]string{
 	"panic": color.RedString("PANIC"),
 }
 
+// keyValue represents phuslu log key/value pair type alias
+type keyValue = struct {
+	Key       string
+	Value     string
+	ValueType byte
+}
+
 // Logger represents wrapper over logging library
 type Logger struct {
-	writer *pLogger.MultiEntryWriter
-	*pLogger.Logger
+	writer *pLog.MultiEntryWriter
+	*pLog.Logger
 }
 
 // New returns new configured logger with log level <lvl>
-func New(lvl pLogger.Level) *Logger {
-	writer := pLogger.MultiEntryWriter{
-		&pLogger.ConsoleWriter{
-			Formatter: newConsoleFormatter(true, "2006.01.02 15:04:05"),
+func New(lvl pLog.Level) *Logger {
+	writer := pLog.MultiEntryWriter{
+		&pLog.ConsoleWriter{
+			Formatter: newConsoleFormatter(true, time.DateTime),
 			Writer:    os.Stderr,
 		},
 	}
-
-	log := pLogger.Logger{
+	log := pLog.Logger{
 		Level:  lvl,
 		Writer: &writer,
 	}
-
 	return &Logger{Logger: &log, writer: &writer}
+}
+
+// Trace prints trace level <msg> with caller
+func (l Logger) Trace(msg string) {
+	l.Logger.Trace().Caller(2).Msg(msg)
+}
+
+// Tracef prints trace level message from <args> in given <format>
+func (l Logger) Tracef(format string, args ...any) {
+	l.Logger.Trace().Caller(2).Msgf(format, args...)
+}
+
+// TraceFi prints trace level <msg> with caller and formatted and colored <fields>
+func (l Logger) TraceFi(msg string, fields ...any) {
+	print(l.Logger.Trace().Caller(2), msg, fields)
+}
+
+// Debug prints debug level <msg> with caller
+func (l Logger) Debug(msg string) {
+	l.Logger.Debug().Caller(2).Msg(msg)
+}
+
+// Debugf prints debug level message from <args> in given <format>
+func (l Logger) Debugf(format string, args ...any) {
+	l.Logger.Debug().Caller(2).Msgf(format, args...)
+}
+
+// DebugFi prints debug level <msg> with caller and formatted and colored <fields>
+func (l Logger) DebugFi(msg string, fields ...any) {
+	print(l.Logger.Debug().Caller(2), msg, fields)
+}
+
+// Info prints info level <msg>
+func (l Logger) Info(msg string) {
+	l.Logger.Info().Msg(msg)
+}
+
+// Infof prints info level message from <args> in given <format>
+func (l Logger) Infof(format string, args ...any) {
+	l.Logger.Info().Msgf(format, args...)
+}
+
+// InfoFi prints info level <msg> with formatted and colored <fields>
+func (l Logger) InfoFi(msg string, fields ...any) {
+	print(l.Logger.Info(), msg, fields)
+}
+
+// Warn prints warning level <msg>
+func (l Logger) Warn(msg string) {
+	l.Logger.Warn().Msg(msg)
+}
+
+// Warnf prints warning level message from <args> in given <format>
+func (l Logger) Warnf(format string, args ...any) {
+	l.Logger.Warn().Msgf(format, args...)
+}
+
+// WarnFi prints warning level <msg> with formatted and colored <fields>
+func (l Logger) WarnFi(msg string, fields ...any) {
+	print(l.Logger.Warn(), msg, fields)
+}
+
+// Error prints error level <msg>
+func (l Logger) Error(msg string) {
+	l.Logger.Error().Msg(msg)
+}
+
+// Errorf prints error level message from <args> in given <format>
+func (l Logger) Errorf(format string, args ...any) {
+	l.Logger.Error().Msgf(format, args...)
+}
+
+// ErrorFi prints error level <msg> with formatted and colored <fields>
+func (l Logger) ErrorFi(msg string, fields ...any) {
+	print(l.Logger.Error(), msg, fields)
+}
+
+// Fatal prints fatal level <msg> and exits the program
+func (l Logger) Fatal(msg string) {
+	l.Logger.Fatal().Msg(msg)
+}
+
+// Fatalf prints fatal level message from <args> in given <format> and exits the program
+func (l Logger) Fatalf(format string, args ...any) {
+	l.Logger.Fatal().Msgf(format, args...)
+}
+
+// FatalFi prints fatal level <msg> with formatted and colored <fields> and exits the program
+func (l Logger) FatalFi(msg string, fields ...any) {
+	print(l.Logger.Fatal(), msg, fields)
+}
+
+// Panic prints panic level <msg> and panics
+func (l Logger) Panic(msg string) {
+	l.Logger.Panic().Msg(msg)
+}
+
+// Panicf prints panic level message from <args> in given <format> and panics
+func (l Logger) Panicf(format string, args ...any) {
+	l.Logger.Panic().Msgf(format, args...)
+}
+
+// PanicFi prints panic level <msg> with formatted and colored <fields> and panics
+func (l Logger) PanicFi(msg string, fields ...any) {
+	print(l.Logger.Panic(), msg, fields)
 }
 
 // AddFileWriter creates log file at <filePath> and adds file writer to logger.
 //
 // If <filePath> is empty string, it does nothing and returns nil error.
-func (l *Logger) AddFileWriter(filePath string) (*os.File, error) {
+func (l Logger) AddFileWriter(filePath string) (*os.File, error) {
 	if filePath == "" {
 		return nil, nil
 	}
@@ -59,18 +169,31 @@ func (l *Logger) AddFileWriter(filePath string) (*os.File, error) {
 	if err != nil {
 		return nil, errors.Wrap(err, "Open or create log file")
 	}
-	*l.writer = append(*l.writer, &pLogger.IOWriter{
+	*l.writer = append(*l.writer, &pLog.IOWriter{
 		Writer: logFile,
 	})
 
 	return logFile, nil
 }
 
+// print adds message <msg> and <fields> to <entry> and prints it
+func print(entry *pLog.Entry, msg string, fields []any) {
+	var key string
+	for i, field := range fields {
+		if i%2 == 0 {
+			key = fmt.Sprint(field)
+		} else {
+			entry.Any(key, field)
+		}
+	}
+	entry.Msg(msg)
+}
+
 // newConsoleFormatter returns formatter funtion with <timeFormat> for console writer.
 //
 // If <colorize> is true, add colors to output.
-func newConsoleFormatter(colorize bool, timeFormat string) func(io.Writer, *pLogger.FormatterArgs) (int, error) {
-	return func(w io.Writer, a *pLogger.FormatterArgs) (int, error) {
+func newConsoleFormatter(colorize bool, timeFormat string) func(io.Writer, *pLog.FormatterArgs) (int, error) {
+	return func(w io.Writer, a *pLog.FormatterArgs) (int, error) {
 		gray := color.RGB(118, 118, 118).SprintFunc()
 		var messageSb strings.Builder
 
@@ -94,25 +217,24 @@ func newConsoleFormatter(colorize bool, timeFormat string) func(io.Writer, *pLog
 		messageSb.WriteRune(' ')
 
 		if a.Caller != "" {
-			messageSb.WriteRune('[')
 			if colorize {
 				messageSb.WriteString(gray(a.Caller))
 			} else {
 				messageSb.WriteString(a.Caller)
 			}
-			messageSb.WriteString("] ")
+			messageSb.WriteRune(' ')
 		}
 
 		messageSb.WriteString(a.Message)
 
-		a.KeyValues = lo.Reject(a.KeyValues, func(kv struct{Key string; Value string; ValueType byte}, _ int) bool {
+		a.KeyValues = lo.Reject(a.KeyValues, func(kv keyValue, _ int) bool {
 			return kv.Key == "callerfunc"
 		})
 		if len(a.KeyValues) > 0 {
 			messageSb.WriteString(": ")
 		}
 		for idx, item := range a.KeyValues {
- 			messageSb.WriteString(item.Key + " \"")
+			messageSb.WriteString(item.Key + " \"")
 			if colorize {
 				messageSb.WriteString(color.CyanString(item.Value))
 			} else {
--- src/util/logger/logger_test.go
+++ src/util/logger/logger_test.go
@@ -5,41 +5,234 @@ import (
 	"encoding/json"
 	"io"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"regexp"
 	"testing"
 
-	pLogger "github.com/phuslu/log"
+	pLog "github.com/phuslu/log"
 	"github.com/stretchr/testify/assert"
 	"github.com/zenizh/go-capturer"
 )
 
+var timeRx = `[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}`
+
 func TestNew(t *testing.T) {
 	out := capturer.CaptureStderr(func() {
-		log := New(pLogger.DebugLevel)
-		log.Trace().Caller(1).Msg("message")
-		log.Debug().Caller(1).Msg("message")
-		log.Debug().Caller(1).Str("k", "v").Msg("message")
-		log.Info().Str("k", "v").Msg("message")
-		log.Warn().Msg("message")
-		log.Error().Msg("message")
-		assert.Panics(t, func() { log.Panic().Msg("message") }, "should panic")
+		log := New(pLog.DebugLevel)
+		log.Trace("message")
+		log.Debug("message")
+		log.DebugFi("message", "k", "v")
+		log.InfoFi("message", "1", 2)
+		log.Warn("message")
+		log.Error("message")
+		assert.Panics(t, func() { log.Panic("message") }, "should panic")
 	})
-	msg := "should not print trace messages with debug level logger"
-	timeRx := `[0-9]{4}\.[0-9]{2}\.[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}`
-	assert.NotRegexp(t, regexp.MustCompile(timeRx + `TRACE \[logger\/logger_test\.go:20\] message`), out, msg)
-	assert.Regexp(t, regexp.MustCompile(timeRx + ` DEBUG \[logger\/logger_test\.go:21\] message`), out)
-	assert.Regexp(t, regexp.MustCompile(timeRx + ` DEBUG \[logger\/logger_test\.go:22\] message: k "v"`), out)
-	assert.Regexp(t, regexp.MustCompile(timeRx + ` INFO message: k "v"`), out)
-	assert.Regexp(t, regexp.MustCompile(timeRx + ` WARN message`), out)
-	assert.Regexp(t, regexp.MustCompile(timeRx + ` ERROR message`), out)
-	assert.Regexp(t, regexp.MustCompile(timeRx + ` PANIC message`), out)
+	assert.NotRegexp(t, regexp.MustCompile(`TRACE`), out, "should not print trace messages with debug level logger")
+	assert.Regexp(t, regexp.MustCompile(timeRx+` DEBUG logger\/logger_test\.go:24 message`), out)
+	assert.Regexp(t, regexp.MustCompile(timeRx+` DEBUG logger\/logger_test\.go:25 message: k "v"`), out)
+	assert.Regexp(t, regexp.MustCompile(timeRx+` INFO message: 1 "2"`), out)
+	assert.Regexp(t, regexp.MustCompile(timeRx+` WARN message`), out)
+	assert.Regexp(t, regexp.MustCompile(timeRx+` ERROR message`), out)
+	assert.Regexp(t, regexp.MustCompile(timeRx+` PANIC message`), out)
+}
+
+func TestTrace(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.TraceLevel)
+		log.Trace("message")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` TRACE logger\/logger_test\.go:43 message`), out)
+}
+
+func TestTracef(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.TraceLevel)
+		log.Tracef("%v + %v", "message 1", "message 2")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` TRACE logger\/logger_test\.go:51 message 1 \+ message 2`), out)
+}
+
+func TestTraceFi(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.TraceLevel)
+		log.TraceFi("message", "a", "b", 1, 2)
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` TRACE logger\/logger_test\.go:59 message: a "b", 1 "2"`), out)
+}
+
+func TestDebug(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		log.Debug("message")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` DEBUG logger\/logger_test\.go:67 message`), out)
+}
+
+func TestDebugf(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		log.Debugf("%v + %v", "message 1", "message 2")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` DEBUG logger\/logger_test\.go:75 message 1 \+ message 2`), out)
+}
+
+func TestDebugFi(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		log.DebugFi("message", "a", "b", 1, 2)
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` DEBUG logger\/logger_test\.go:83 message: a "b", 1 "2"`), out)
+}
+
+func TestInfo(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		log.Info("message")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` INFO message`), out)
+}
+
+func TestInfof(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		log.Infof("%v + %v", "message 1", "message 2")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` INFO message 1 \+ message 2`), out)
+}
+
+func TestInfoFi(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		log.InfoFi("message", "a", "b", 1, 2)
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` INFO message: a "b", 1 "2"`), out)
+}
+
+func TestWarn(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		log.Warn("message")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` WARN message`), out)
+}
+
+func TestWarnf(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		log.Warnf("%v + %v", "message 1", "message 2")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` WARN message 1 \+ message 2`), out)
+}
+
+func TestWarnFi(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		log.WarnFi("message", "a", "b", 1, 2)
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` WARN message: a "b", 1 "2"`), out)
+}
+
+func TestError(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		log.Error("message")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` ERROR message`), out)
+}
+
+func TestErrorf(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		log.Errorf("%v + %v", "message 1", "message 2")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` ERROR message 1 \+ message 2`), out)
+}
+
+func TestErrorFi(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		log.ErrorFi("message", "a", "b", 1, 2)
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` ERROR message: a "b", 1 "2"`), out)
+}
+
+func TestFatal(t *testing.T) {
+	if os.Getenv("TestFatal") == "1" {
+		log := New(pLog.DebugLevel)
+		log.Fatal("message")
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=^TestFatal$")
+	cmd.Env = append(os.Environ(), "TestFatal=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		assert.Exactly(t, 255, e.ExitCode(), "should exit with that exit code")
+		return
+	}
+	assert.Failf(t, "", "expected exec.ExitError, got '%v'", err)
+}
+
+func TestFatalf(t *testing.T) {
+	if os.Getenv("TestFatal") == "1" {
+		log := New(pLog.DebugLevel)
+		log.Fatalf("%v + %v", "message 1", "message 2")
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=^TestFatal$")
+	cmd.Env = append(os.Environ(), "TestFatal=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		assert.Exactly(t, 255, e.ExitCode(), "should exit with that exit code")
+		return
+	}
+	assert.Failf(t, "", "expected exec.ExitError, got '%v'", err)
+}
+
+func TestFatalFi(t *testing.T) {
+	if os.Getenv("TestFatal") == "1" {
+		log := New(pLog.DebugLevel)
+		log.FatalFi("message", "a", "b", 1, 2)
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=^TestFatal$")
+	cmd.Env = append(os.Environ(), "TestFatal=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		assert.Exactly(t, 255, e.ExitCode(), "should exit with that exit code")
+		return
+	}
+	assert.Failf(t, "", "expected exec.ExitError, got '%v'", err)
+}
+
+func TestPanic(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		assert.Panics(t, func() { log.Panic("message") }, "should panic")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` PANIC message`), out)
+}
+
+func TestPanicf(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		assert.Panics(t, func() { log.Panicf("%v + %v", "message 1", "message 2") }, "should panic")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` PANIC message 1 \+ message 2`), out)
+}
+
+func TestPanicFi(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		assert.Panics(t, func() { log.PanicFi("message", "a", "b", 1, 2) }, "should panic")
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` PANIC message: a "b", 1 "2"`), out)
 }
 
 func TestAddFileWriter(t *testing.T) {
 	path := filepath.Join(t.TempDir(), "test.log")
 
-	log := New(pLogger.DebugLevel)
+	log := New(pLog.DebugLevel)
 	file, err := log.AddFileWriter("")
 	assert.NoError(t, err, "should not return error for empty file path")
 	assert.Nil(t, file, "should return nil file for empty file path")
@@ -49,8 +242,8 @@ func TestAddFileWriter(t *testing.T) {
 	assert.NotNil(t, file, "should create object")
 	assert.FileExists(t, path, "should create log file at given path")
 
-	log.Info().Str("field1", "value 1").Str("field2", "value 2").Msg("message 1")
-	log.Warn().Str("field1", "value 3").Str("field2", "value 4").Msg("message 2")
+	log.InfoFi("message 1", "field1", "value 1", "field2", 2)
+	log.WarnFi("message 2", "field1", "value 3", "field2", 4)
 
 	// file.Sync() does not help, content is empty, closing and opening the file again
 	file.Close()
@@ -63,7 +256,7 @@ func TestAddFileWriter(t *testing.T) {
 		Time    string `json:"time"`
 		Level   string `json:"level"`
 		Field1  string `json:"field1"`
-		Field2  string `json:"field2"`
+		Field2  int    `json:"field2"`
 		Message string `json:"message"`
 	}
 
@@ -77,7 +270,7 @@ func TestAddFileWriter(t *testing.T) {
 	assert.Regexp(t, timeRx, entry.Time, "time in entry must match regexp format")
 	assert.Exactly(t, "info", entry.Level, "log severity must be that level")
 	assert.Exactly(t, "value 1", entry.Field1, "field should have this value")
-	assert.Exactly(t, "value 2", entry.Field2, "field should have this value")
+	assert.Exactly(t, 2, entry.Field2, "field should have this value")
 	assert.Exactly(t, "message 1", entry.Message, "should be that entry message")
 
 	line, err = reader.ReadBytes('\n')
@@ -87,12 +280,26 @@ func TestAddFileWriter(t *testing.T) {
 	assert.Regexp(t, timeRx, entry.Time, "time in entry must match regexp format")
 	assert.Exactly(t, "warn", entry.Level, "log severity must be that level")
 	assert.Exactly(t, "value 3", entry.Field1, "field should have this value")
-	assert.Exactly(t, "value 4", entry.Field2, "field should have this value")
+	assert.Exactly(t, 4, entry.Field2, "field should have this value")
 	assert.Exactly(t, "message 2", entry.Message, "should be that entry message")
+}
+
+func TestPrint(t *testing.T) {
+	out := capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		print(log.Logger.Info(), "message", nil)
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` INFO message`), out)
+
+	out = capturer.CaptureStderr(func() {
+		log := New(pLog.DebugLevel)
+		print(log.Logger.Info(), "message", []any{"a", "b", 1, 2})
+	})
+	assert.Regexp(t, regexp.MustCompile(timeRx+` INFO message: a "b", 1 "2"`), out)
 }
 
 func TestNewConsoleFormatter(t *testing.T) {
 	// Tested in TestNew
-	var formatter func(io.Writer, *pLogger.FormatterArgs) (int, error)
+	var formatter func(io.Writer, *pLog.FormatterArgs) (int, error)
 	assert.IsType(t, formatter, newConsoleFormatter(false, ""), "formatter function should have this definition")
 }
